695574b1a1a9d0d8b1bdc2a9fae3859b60191d5b
Grégory <gregory.brugnet@gmail.com>
2020-07-15 13:39:03 +0200

    better sql

---
+++ ESGI.DesignPattern.Projet/DbConnection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESGI.DesignPattern.Projet
+{
+    class DbConnection
+    {
+    }
+}
--- ESGI.DesignPattern.Projet/ReceiptRepository.cs
+++ ESGI.DesignPattern.Projet/ReceiptRepository.cs
@@ -5,33 +5,35 @@ using System.Runtime.Serialization;
 
 namespace ESGI.DesignPattern.Projet
 {
+    
     public class ReceiptRepository : IReceiptRepository
     {
-        private static readonly string databaseName = "myshop";
-        private static readonly string user = "store";
-        private static readonly string pass = "123456";
-        public void Delete(int id)
+        //private static readonly string databaseName = "myshop";
+        //private static readonly string user = "store";
+        //private static readonly string pass = "123456";
+
+        private static DbConnection dbConnection;
+        
+
+    public void Delete(int id)
         {
             throw new NotImplementedException();
         }
 
         public void Insert(Receipt receipt)
-        {
-            using (var connection = new MySqlConnection
             {
-                ConnectionString = $"Database={databaseName};Data Source=localhost;User Id={user};Password={pass}"
-            })
-            {
-                connection.Open();
-                var command = new MySqlCommand("insert into RECEIPT (AMOUNT, TAX, TOTAL)"
-                        + "values(@amount, @tax, @total);", connection);
-                command.Parameters.AddWithValue("@amount", receipt.Amount.AsDecimal());
-                command.Parameters.AddWithValue("@tax", receipt.Tax.AsDecimal());
-                command.Parameters.AddWithValue("@total", receipt.Total.AsDecimal());
-                command.Prepare();
-                command.ExecuteNonQuery();
+                if (dbConnection.IsConnect())
+                    {
+                        var command = new MySqlCommand("insert into RECEIPT (AMOUNT, TAX, TOTAL)"
+                                            + "values(@amount, @tax, @total);", dbConnection.Connection);
+                        command.Parameters.AddWithValue("@amount", receipt.Amount.AsDecimal());
+                        command.Parameters.AddWithValue("@tax", receipt.Tax.AsDecimal());
+                        command.Parameters.AddWithValue("@total", receipt.Total.AsDecimal());
+                        command.Prepare();
+                        command.ExecuteNonQuery();
+                    }
+                dbConnection.Close(); 
             }
-        }
 
         public void Update(Receipt receipt)
         {
